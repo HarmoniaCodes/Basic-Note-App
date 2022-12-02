e2cdb5a29766d261ca68478018502b10ed2ed641
Nick <nickwillcode@pm.me>
2022-12-02 13:18:45 -0500

    static vers

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import "../App.css";
+import Header from "./header";
+import Note from "./note";
+import Footer from "./footer";
+
+function App() {
+  return (
+    <main>
+      <Header />
+      <Note />
+      <Footer />
+    </main>
+  );
+}
+
+export default App;
